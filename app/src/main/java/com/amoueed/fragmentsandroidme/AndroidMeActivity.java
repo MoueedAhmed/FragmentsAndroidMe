238840203b3e9ca6c92ecdeade3f5f0c04891197
Rao Moueed Ahmed <moueedrao@gmail.com>
2020-06-15 13:09:37 +0500

    TFragments.01-Solution-CreateBodyPartFragment

--- app/src/main/java/com/amoueed/fragmentsandroidme/AndroidMeActivity.java
+++ app/src/main/java/com/amoueed/fragmentsandroidme/AndroidMeActivity.java
@@ -1,6 +1,7 @@
 package com.amoueed.fragmentsandroidme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,15 @@ public class AndroidMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+
+        // Create a new head BodyPartFragment
+        BodyPartFragment headFragment = new BodyPartFragment();
+
+        // Add the fragment to its container using a FragmentManager and a Transaction
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, headFragment)
+                .commit();
     }
 }
---
+++ app/src/main/java/com/amoueed/fragmentsandroidme/BodyPartFragment.java
@@ -0,0 +1,40 @@
+package com.amoueed.fragmentsandroidme;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.amoueed.fragmentsandroidme.data.AndroidImageAssets;
+
+public class BodyPartFragment extends Fragment {
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the fragment
+     */
+    public BodyPartFragment() {
+    }
+
+    /**
+     * Inflates the fragment layout file and sets the correct resource for the image to display
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the Android-Me fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        // Get a reference to the ImageView in the fragment layout
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        // Set the image to the first in our list of head images
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        // Return the rootView
+        return rootView;
+    }
+
+}
