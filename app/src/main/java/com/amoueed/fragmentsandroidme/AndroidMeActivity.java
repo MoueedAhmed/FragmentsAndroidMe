65564142dd9f227b54bfbd7d76b3eda9fcd71de1
Rao Moueed Ahmed <moueedrao@gmail.com>
2020-06-15 12:51:54 +0500

    nearly equal to udacity androidme course startrup app

--- app/src/main/java/com/amoueed/fragmentsandroidme/AndroidMeActivity.java
+++ app/src/main/java/com/amoueed/fragmentsandroidme/AndroidMeActivity.java
@@ -9,6 +9,6 @@ public class AndroidMeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_android_me);
     }
 }
